ec64fb2041d2cb163d67947582284f144a31c9d7
Manuel Gerding <manuel@steadybit.com>
2021-03-18 15:36:22 +0100

    feat: Adding error details (e.g. kill switch)

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import { SteadybitAPI } from './steadybitAPI.js';
 
 import core from '@actions/core';
 
+const getReasonDetails = (reason) => {
+    if (reason.response && reason.response.data) {
+        if (reason.response.data.title) {
+            return reason.response.data.title;
+        }
+        return JSON.stringify(reason.response.data);
+    }
+    return null;
+};
+
 function run() {
     const baseURL = core.getInput('baseURL');
     const apiAccessToken = core.getInput('apiAccessToken');
@@ -12,7 +22,8 @@ function run() {
     const steadybitAPI = new SteadybitAPI(baseURL, apiAccessToken);
     steadybitAPI.executeExperiment(experimentKey, expectedState, expectedFailureReason)
         .catch(reason => {
-            core.setFailed(`Experiment ${experimentKey} failed: ${reason}` );
+            const reasonDetails = getReasonDetails(reason);
+            core.setFailed(`Experiment ${experimentKey} failed: ${reason}${reasonDetails ? `, Details: ${reasonDetails}` : ''}`);
         });
 }
 
