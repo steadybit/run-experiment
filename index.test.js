04e7aa0d91c6724dc82ba2162fcae261cfaa09ad
Daniel Reuter <1879911+ReuDa@users.noreply.github.com>
2021-10-01 09:22:00 +0200

    feat: renamed api (scenario to experiment)

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const {SteadybitAPI} = require('./steadybitAPI.js');
+const { SteadybitAPI } = require('./steadybitAPI.js');
 
 describe('SteadybitAPI', () => {
     const httpMock = {
         post: jest.fn(),
-        get: jest.fn()
+        get: jest.fn(),
     };
     const api = new SteadybitAPI('http://test', 'XXX', () => httpMock);
     api.allowParallelBackoffInterval = 0.001;
@@ -19,7 +19,7 @@ describe('SteadybitAPI', () => {
         const url = await api.runExperiment('EX-1');
 
         expect(url).toBe('http://test/api/executions/123');
-        expect(httpMock.post).toHaveBeenCalledWith(`/api/scenarios/EX-1/run`, null, { params: { allowParallel: 'false' } });
+        expect(httpMock.post).toHaveBeenCalledWith(`/api/experiments/EX-1/execute`, null, { params: { allowParallel: 'false' } });
     });
 
     it('should throw on failed experiment run', async () => {
@@ -41,14 +41,13 @@ describe('SteadybitAPI', () => {
         expect(httpMock.post).toHaveBeenCalledTimes(3);
     });
 
-
     it('should await execution state successfully', async () => {
         httpMock.get.mockResolvedValueOnce({ data: { id: 123, state: 'RUNNING' } });
         httpMock.get.mockResolvedValueOnce({ data: { id: 123, state: 'COMPLETED', ended: '2021-09-24T12:35:00Z' } });
 
         const result = await api.awaitExecutionState('http://test/api/executions/123', 'COMPLETED');
 
-        expect(result).toBe('Execution 123 ended with \'COMPLETED\'.');
+        expect(result).toBe("Execution 123 ended with 'COMPLETED'.");
         expect(httpMock.get).toHaveBeenCalledWith(`http://test/api/executions/123`);
         expect(httpMock.get).toHaveBeenCalledTimes(2);
     });
@@ -56,6 +55,8 @@ describe('SteadybitAPI', () => {
     it('should await execution state unsuccessfully', async () => {
         httpMock.get.mockResolvedValue({ data: { id: 123, state: 'FAILED', ended: '2021-09-24T12:35:00Z', failureReason: 'test' } });
 
-        await expect(api.awaitExecutionState('http://test/api/executions/123', 'COMPLETED')).rejects.toBe('Execution 123 ended with \'FAILED - test\' but expected \'COMPLETED\'');
+        await expect(api.awaitExecutionState('http://test/api/executions/123', 'COMPLETED')).rejects.toBe(
+            "Execution 123 ended with 'FAILED - test' but expected 'COMPLETED'"
+        );
     });
 });
